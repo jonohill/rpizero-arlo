1e342928f86f14f45954da4329a2c51899dce881
Jono Hill <jonathan@hillnz.com>
2020-02-08 16:23:21 +1300

    change config

--- receiver/video_server.py
+++ receiver/video_server.py
@@ -10,32 +10,38 @@ from pprint import pformat
 log = logging.getLogger(__name__)
 logging.basicConfig(level=os.environ.get('LOGLEVEL', 'WARNING').upper())
 
-# All environment variables begin with MZ_
-def get_env(name, default=None, val_type=str):
-    if type(default) != str and val_type == str:
-        val_type = type(default)
-    env_name = 'MZ_' + name
-    val = os.environ.get(env_name, default) or default
-    if val is None:
-        raise KeyError(f'Environment variable "{env_name}" is required')
-    if type(val) == str:
-        if val_type == list or val_type == set:
-            val = [ v.strip() for v in val.strip().split(',') ]
-        if val_type == set:
-            val = set(val)
-    return val
+class MzConfig:
 
-POST_KEY = get_env('POST_KEY', str(uuid()))
-if POST_KEY:
-    print(f'The POST key is {POST_KEY}')
-IFTTT_ENDPOINT = get_env('IFTTT_ENDPOINT', '')
-IFTTT_KEY = get_env('IFTTT_KEY')
-FRAME_URL_BASE = get_env('URL_BASE')
-FRAMES_DIR = get_env('FRAMES_DIR', '/tmp/frames')
-SAVE_DIR = get_env('SAVE_DIR', '')
-YOLO_CONF_DIR = get_env('YOLO_CONF_DIR', 'yolo')
-OBJECT_WHITELIST = get_env('OBJECT_WHITELIST', set())
-OBJECT_BLACKLIST = get_env('OBJECT_BLACKLIST', set())
+    def __init__(self):
+        self.post_key = self.get_env('POST_KEY', str(uuid()))
+        self.ifttt_endpoint = self.get_env('IFTTT_ENDPOINT', '')
+        self.ifttt_key = self.get_env('IFTTT_KEY')
+        self.frame_url_base = self.get_env('URL_BASE')
+        self.frames_dir = self.get_env('FRAMES_DIR', '/tmp/frames')
+        self.save_dir = self.get_env('SAVE_DIR', '')
+        self.yolo_conf_dir = self.get_env('YOLO_CONF_DIR', 'yolo')
+        self.object_whitelist = self.get_env('OBJECT_WHITELIST', set())
+        self.object_blacklist = self.get_env('OBJECT_BLACKLIST', set())
+
+    # All environment variables begin with MZ_
+    def get_env(self, name, default=None, val_type=str):
+        if type(default) != str and val_type == str:
+            val_type = type(default)
+        env_name = 'MZ_' + name
+        val = os.environ.get(env_name, default) or default
+        if val is None:
+            raise KeyError(f'Environment variable "{env_name}" is required')
+        if type(val) == str:
+            if val_type == list or val_type == set:
+                val = [ v.strip() for v in val.strip().split(',') ]
+            if val_type == set:
+                val = set(val)
+        return val
+
+config = MzConfig()
+log.debug(vars(config))
+if config.post_key:
+    print(f'The POST key is {config.post_key}')
 
 routes = web.RouteTableDef()
 
@@ -44,14 +50,14 @@ NUM_FRAMES = 3
 @routes.post('/video')
 async def post_video(request: web.Request):
     # Validate key
-    if request.headers.get('x-key', None) != POST_KEY:
+    if request.headers.get('x-key', None) != config.post_key:
         return web.Response(status=403)
         
     try:
-        kwargs = { 'ifttt_key': IFTTT_KEY, 'frame_dir': FRAMES_DIR, 'frame_url_base': FRAME_URL_BASE, 
-                   'video_dir': SAVE_DIR, 'yolo_conf_dir': YOLO_CONF_DIR, 'blacklist': OBJECT_BLACKLIST, 'whitelist': OBJECT_WHITELIST }
-        if IFTTT_ENDPOINT:
-            kwargs['ifttt_endpoint'] = IFTTT_ENDPOINT
+        kwargs = { 'ifttt_key': config.ifttt_key, 'frame_dir': config.frames_dir, 'frame_url_base': config.frame_url_base, 
+                   'video_dir': config.save_dir, 'yolo_conf_dir': config.yolo_conf_dir, 'blacklist': config.object_blacklist, 'whitelist': config.object_whitelist }
+        if config.ifttt_endpoint:
+            kwargs['ifttt_endpoint'] = config.ifttt_endpoint
         async with VideoNotifier(**kwargs) as notifier:
             await notifier.check_video(request.content)
     except Exception as err:
@@ -72,8 +78,8 @@ async def post_mock_ifttt(request: web.Request):
         log.debug(f'Mock - exception {err}')
         raise err
 
-os.makedirs(FRAMES_DIR, exist_ok=True)
-routes.static('/frames', FRAMES_DIR)
+os.makedirs(config.frames_dir, exist_ok=True)
+routes.static('/frames', config.frames_dir)
 
 app = web.Application()
 app.add_routes(routes)
